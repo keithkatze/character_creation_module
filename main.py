ebc899d823666e46422fd28cd0e66c35d7545716
kate_moroz97@mail.ru <kate_moroz97@mail.ru>
2023-01-29 22:19:05 +0300

    Add docstrings main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+"""Модуль выбора и тренировки персонажа выбранного класса."""
+
 from random import randint
+
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
@@ -90,8 +94,9 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> str:
+def main() -> None:
     """Начало игры и приветствие."""
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -105,3 +110,16 @@ def main() -> str:
 
 
 main()
+
+
+if __name__ == '__main__':
+    run_screensaver()  # Динамическая заставка в начале игры.
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class))
